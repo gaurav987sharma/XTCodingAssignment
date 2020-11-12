81a98d343a1542f9a8f011d00f447754f61bee94
gaurav987sharma <73336838+gaurav987sharma@users.noreply.github.com>
2020-11-12 14:53:49 +0530

    change ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import "./styles.css";
-import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
-import CardMedia from "@material-ui/core/CardMedia";
+import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
-const style1 = {
-  root: {
-    maxWidth: 345,
-    flexGrow: 1
-  },
-  media: {
-    height: 140
-  }
-};
+import "./styles.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -59,7 +49,7 @@ class App extends React.Component {
     for (let key in obj) {
       str += `&${key}=${obj[key]}`;
     }
-    const Url = "https://api.spaceXdata.com/v3/launches?limit=100" + str;
+    const Url = "https://api.spaceXdata.com/v3/launches?limit=50" + str;
     fetch(Url)
       .then((response) => response.json())
       .then((data) =>
@@ -88,7 +78,7 @@ class App extends React.Component {
   }
   componentDidUpdate() {}
   render() {
-    const { classes } = this.props;
+    const center = { justifyContent: "center", alignItems: "center" };
     const { data } = this.state;
     const yearArr = [
       2006,
@@ -110,204 +100,248 @@ class App extends React.Component {
     console.log(this.state);
     return (
       <>
-        <Grid container spacing={3}>
-          <Grid item xs={2} sm={2}>
-            <Grid container spacing={3}>
-              <Card className={classes.root}>
-                <CardActionArea>
-                  <CardContent>
-                    <Typography gutterBottom variant="h4" component="h3">
-                      Filters
-                    </Typography>
-                    <Typography
-                      gutterBottom
-                      style={{ textDecorationLine: "underline" }}
+        <Container fixed>
+          <Typography gutterBottom variant="h4" component="h3">
+            SpaceX Launch Programs
+          </Typography>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <Grid
+                container
+                spacing={1}
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: "100vh" }}
+              >
+                <Card className={"root"}>
+                  <CardActionArea>
+                    <CardContent>
+                      <Typography gutterBottom variant="h4" component="h3">
+                        Filters
+                      </Typography>
+                      <Typography align="center">Launch Year</Typography>
+                      <Typography align="center">
+                        --------------------------------------
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="center"
+                      alignItems="center"
+                      spacing={1}
                     >
-                      Launch Year
+                      {yearArr.map((item) => (
+                        <Grid item xs={6} className="maxwidth100">
+                          <Button
+                            variant="contained"
+                            size="small"
+                            color="primary"
+                            style={
+                              this.state.year == item
+                                ? { backgroundColor: "#155015" }
+                                : { backgroundColor: "green" }
+                            }
+                            className={"button"}
+                            onClick={this.FilterHandler}
+                            name="year"
+                            component="button"
+                            value={item}
+                          >
+                            {item}
+                          </Button>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </CardActions>
+                  <CardContent>
+                    <Typography align="center">Successful Launch</Typography>
+                    <Typography align="center">
+                      --------------------------------------
                     </Typography>
                   </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Grid container spacing={3}>
-                    {yearArr.map((item) => (
-                      <Grid item xs={12} sm={6}>
+                  <CardActions>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="center"
+                      alignItems="center"
+                      spacing={1}
+                    >
+                      <Grid item xs={6} className="maxwidth90">
                         <Button
                           variant="contained"
                           size="small"
                           color="primary"
                           style={
-                            this.state.year == item
+                            this.state.launchStatus == "true"
                               ? { backgroundColor: "#155015" }
                               : { backgroundColor: "green" }
                           }
-                          className={classes.button}
+                          className={"margin"}
                           onClick={this.FilterHandler}
-                          name="year"
-                          component="button"
-                          value={item}
+                          value="true"
+                          name="launchStatus"
                         >
-                          {item}
+                          True
                         </Button>
                       </Grid>
-                    ))}
-                  </Grid>
-                </CardActions>
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    style={{ textDecorationLine: "underline" }}
-                  >
-                    Successful Launch
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6}>
-                      <Button
-                        variant="contained"
-                        size="small"
-                        color="primary"
-                        style={
-                          this.state.launchStatus == "true"
-                            ? { backgroundColor: "#155015" }
-                            : { backgroundColor: "green" }
-                        }
-                        className={classes.margin}
-                        onClick={this.FilterHandler}
-                        value="true"
-                        name="launchStatus"
-                      >
-                        True
-                      </Button>
+                      <Grid item xs={6} style={{ maxWidth: "80px" }}>
+                        <Button
+                          variant="contained"
+                          size="small"
+                          color="primary"
+                          style={
+                            this.state.launchStatus == "false"
+                              ? { backgroundColor: "#155015" }
+                              : { backgroundColor: "green" }
+                          }
+                          className={"margin"}
+                          onClick={this.FilterHandler}
+                          value="false"
+                          name="launchStatus"
+                        >
+                          False
+                        </Button>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <Button
-                        variant="contained"
-                        size="small"
-                        color="primary"
-                        style={
-                          this.state.launchStatus == "false"
-                            ? { backgroundColor: "#155015" }
-                            : { backgroundColor: "green" }
-                        }
-                        className={classes.margin}
-                        onClick={this.FilterHandler}
-                        value="false"
-                        name="launchStatus"
-                      >
-                        False
-                      </Button>
+                  </CardActions>
+                  <CardContent>
+                    <Typography
+                      style={{ textDecorationLine: "underline" }}
+                      align="center"
+                    >
+                      Successful Landing
+                    </Typography>
+                    <Typography align="center">
+                      --------------------------------------
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="center"
+                      alignItems="center"
+                      spacing={1}
+                    >
+                      <Grid item xs={6} className="maxwidth90">
+                        <Button
+                          variant="contained"
+                          size="small"
+                          color="primary"
+                          style={
+                            this.state.landingSatus == "true"
+                              ? { backgroundColor: "#155015" }
+                              : { backgroundColor: "green" }
+                          }
+                          className={"margin"}
+                          onClick={this.FilterHandler}
+                          value="true"
+                          name="landingSatus"
+                        >
+                          True
+                        </Button>
+                      </Grid>
+                      <Grid item xs={6} className="maxwidth90">
+                        <Button
+                          variant="contained"
+                          size="small"
+                          color="primary"
+                          style={
+                            this.state.landingSatus == "false"
+                              ? { backgroundColor: "#155015" }
+                              : { backgroundColor: "green" }
+                          }
+                          className={"margin"}
+                          onClick={this.FilterHandler}
+                          value="false"
+                          name="landingSatus"
+                        >
+                          False
+                        </Button>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </CardActions>
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    style={{ textDecorationLine: "underline" }}
-                  >
-                    Successful Landing
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6}>
-                      <Button
-                        variant="contained"
-                        size="small"
-                        color="primary"
-                        style={
-                          this.state.landingSatus == "true"
-                            ? { backgroundColor: "#155015" }
-                            : { backgroundColor: "green" }
-                        }
-                        className={classes.margin}
-                        onClick={this.FilterHandler}
-                        value="true"
-                        name="landingSatus"
-                      >
-                        True
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <Button
-                        variant="contained"
-                        size="small"
-                        color="primary"
-                        style={
-                          this.state.landingSatus == "false"
-                            ? { backgroundColor: "#155015" }
-                            : { backgroundColor: "green" }
-                        }
-                        className={classes.margin}
-                        onClick={this.FilterHandler}
-                        value="false"
-                        name="landingSatus"
-                      >
-                        False
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </CardActions>
-              </Card>
-            </Grid>
-          </Grid>
-          {this.state.loader ? (
-            <img src="https://media2.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" />
-          ) : (
-            <Grid item xs={10} sm={10}>
-              <Grid container spacing={3}>
-                {data.map((item, index) => (
-                  <Grid item key={index} xs={9} sm={3}>
-                    <Card className={classes.root}>
-                      <CardActionArea>
-                        <CardMedia
-                          className={classes.media}
-                          style={{ maxWidth: "100%" }}
-                          image={item.links.mission_patch_small}
-                          title=""
-                        />
-                        <CardContent>
-                          <Typography
-                            gutterBottom
-                            style={{ fontWeight: "bold", color: "#17179e" }}
-                          >
-                            {item.mission_name} #{item.flight_number}
-                          </Typography>
-                          <Typography
-                            variant="body2"
-                            color="textSecondary"
-                            component="p"
-                          >
-                            <b>Mission Ids:</b>
-                            <ul>
-                              {item.mission_id.map((item, index) => (
-                                <li key={index}>{item}</li>
-                              ))}
-                            </ul>
-                            <p>
-                              <b>Launch date:</b> {item.launch_year}
-                            </p>
-                            <p>
-                              <b>Successful Launch:</b>{" "}
-                              {item.launch_success.toString()}
-                            </p>
-                            <p>
-                              <b>Successful Landing:</b>{" "}
-                              {item.launch_date_local}
-                            </p>
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                ))}
+                  </CardActions>
+                </Card>
               </Grid>
             </Grid>
-          )}
-        </Grid>
+
+            <Grid item lg={9} md={8} xs={12} sm={6}>
+              {this.state.loader ? (
+                <img
+                  className="width100"
+                  src="https://media2.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
+                />
+              ) : (
+                <Grid
+                  container
+                  alignItems="center"
+                  justify="center"
+                  spacing={1}
+                >
+                  {data.map((item, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+                      <Card className="rootCard">
+                        <CardActionArea>
+                          <img
+                            className="media"
+                            src={item.links.mission_patch_small}
+                          />
+
+                          <CardContent>
+                            <Typography
+                              gutterBottom
+                              className="commaValue title"
+                            >
+                              {item.mission_name} #{item.flight_number}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                              className="commaValue"
+                            >
+                              <b>Mission Ids: </b>
+                              <span>{item.mission_id.join(",")}</span>
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
+                              <b>Launch Year:</b> {item.launch_year}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
+                              <b>Successful Launch:</b>{" "}
+                              {` ${item.launch_success.toString()}`}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
+                              <b>Successful Landing:</b>
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              )}
+            </Grid>
+          </Grid>
+        </Container>
       </>
     );
   }
 }
 
-export default withStyles(style1)(App);
+export default App;
